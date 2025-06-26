2d95302de728dc3a28200c038ac6d1a282fed514
Aweiro <arsenskott@gmail.com>
2025-06-26 16:03:07 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,22 @@
+"use strict";
+const pushNotification = (posTop, posRight, title, description, type)=>{
+    const notification = document.createElement("div");
+    const notificationTitle = document.createElement("h2");
+    const notificationDescription = document.createElement("p");
+    notification.classList.add("notification", type);
+    notificationTitle.className = "title";
+    notificationTitle.textContent = title;
+    notificationDescription.textContent = description;
+    notification.append(notificationTitle, notificationDescription);
+    document.body.appendChild(notification);
+    notification.style.top = `${posTop}px`;
+    notification.style.right = `${posRight}px`;
+    setTimeout(()=>{
+        notification.style.visibility = "hidden";
+    }, 2000);
+};
+pushNotification(10, 10, "Title of Success message", "Message example.\n Notification should contain title and description.", "success");
+pushNotification(150, 10, "Title of Error message", "Message example.\n Notification should contain title and description.", "error");
+pushNotification(290, 10, "Title of Warning message", "Message example.\n Notification should contain title and description.", "warning");
+
+//# sourceMappingURL=index.f75de5e1.js.map
